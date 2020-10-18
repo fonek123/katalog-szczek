7099ee332dbe51a1756eb72c8508251efe3eb616
Fonek <fonek123@gmail.com>
2020-10-18 22:33:01 +0200

    a

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ app.delete("/blogs/edit/:id", function (req, res) {
 // app.get("/blogs/*", function (req, res) {
 //     res.redirect("/blogs");
 // });
-app.listen(process.env.PORT, function () {
-    console.log("Server started");
-});
+// app.listen(process.env.PORT, function () {
+//     console.log("Server started");
+// });
 
